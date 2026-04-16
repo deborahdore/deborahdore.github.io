402c20875314ee64b628e09bcca9ee2b0ed2d958
deborahdore <57449330+deborahdore@users.noreply.github.com>
2026-04-16 07:55:52 +0000

    Deploying to gh-pages from @ deborahdore/deborahdore.github.io@c90b90e4153eaccdd24f014b12f1d773a2ed1567 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "news-i-m-thrilled-to-share-that-i-have-successfully-defended-my-master-s-thesis-leveraging-structural-knowledge-for-argument-mining-in-political-debates-at-sapienza-università-di-roma-earning-a-final-mark-of-110-110-cum-laude",
           title: '🎓 I’m thrilled to share that I have successfully defended my Master’s thesis...',
           description: "",
